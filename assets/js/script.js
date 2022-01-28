1166f391c318a65491c7d711bf8d5ca4663cbea7
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-01-28 01:55:29 -0500

    undo hardcoded testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ var timesArray = [
 
 // Do every second
 setInterval(function () {
-    var currentHourAMPM = '12PM';              // variable for (current hour + AM/PM)
+    var currentHourAMPM = moment().format('hA');              // variable for (current hour + AM/PM)
     var currentHourIDX = timesArray.indexOf(currentHourAMPM); // current hour idx in timesArray
 
     // PAST
