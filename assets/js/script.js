6c5443d5d7174d3421ec793ed1f755e50e135c9d
Yaakov Yisrael Benjamin <yyisrael613@gmail.com>
2022-01-28 01:48:43 -0500

    set/get local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,19 +31,47 @@ var timesArray = [
 
 // Do every second
 setInterval(function () {
-    var currentHourAMPM = moment().format('hA');              // variable for (current hour + AM/PM)
+    var currentHourAMPM = '12PM';              // variable for (current hour + AM/PM)
     var currentHourIDX = timesArray.indexOf(currentHourAMPM); // current hour idx in timesArray
-    
+
     // PAST
     for (var i = 0; i < currentHourIDX; i++) {                // from 12AM - current hour minus 1
         $('#' + timesArray[i]).attr('class', 'col-10 past');  // add 'past' class to textarea
     }
 
     // PRESENT         
-    $('#' + currentHourAMPM).attr('class', 'col-10 present') // add 'present' class to textarea
+    $('#' + currentHourAMPM).attr('class', 'col-10 present')   // add 'present' class to textarea
 
     // FUTURE
     for (var j = currentHourIDX + 1; j < timesArray.length - 1; j++) { // from current hour - 11PM
         $('#' + timesArray[j]).attr('class', 'col-10 future');         // add 'future' class to textarea
     }
 }, 1000)
+
+var eventsArray = []; // create events array
+var eventsIDX = 0;    // initialize array index
+
+// Get events from Local Storage
+eventsArray = JSON.parse(localStorage.getItem("events")) || []; // events: Local Storage -> array
+for (var i = 9; i <= 17; i++) {                                 // iterate over textarea hours 9-5
+    $('textarea#' + timesArray[i]).val(eventsArray[eventsIDX]); // populate textarea values
+    eventsIDX++;
+}
+
+// Save events to Local Storage
+$('.saveBtn').on("click", function () { // when user clicks save...
+    eventsIDX = 0;                      // reset events index
+    for (var i = 9; i <= 17; i++) {     // iterate over textarea hours 9-5
+        if ($('textarea#' + timesArray[i]).val()) { // if textarea has a value
+            eventsArray[eventsIDX] = $('textarea#' + timesArray[i]).val(); // store value in array
+        } else {
+            eventsArray[eventsIDX] = ''; // otherwise store empty string
+        }
+        eventsIDX++; // increment index
+    }
+
+    eventsIDX = 0; // reset index 
+
+    localStorage.setItem("events", JSON.stringify(eventsArray)); // store events in Local Storage
+});
+
